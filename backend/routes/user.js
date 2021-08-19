473395a9369e1b83a942ea025bd56027e3a84a75
Chris VanGroll <chrisvangroll@gmail.com>
2021-08-19 13:17:18 -0500

    Authorization tokens created

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 //const user = require('../models/user');
+//const auth = require('../middleware/auth');
 
 const userCtrl = require('../controllers/user');
 
