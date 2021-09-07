3f44586052b11307b30629d4718e0b0b31984a49
Chris VanGroll <chrisvangroll@gmail.com>
2021-09-07 11:01:29 -0500

    Implemented express bouncer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bouncer = require('express-bouncer')(500, 900000)
 //const user = require('../models/user');
 //const auth = require('../middleware/auth');
 
@@ -8,6 +9,6 @@ const userCtrl = require('../controllers/user');
 const verifyPassword = require('../middleware/verifyPassword')
 
 router.post('/signup', verifyPassword, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', bouncer.block, userCtrl.login);
 
 module.exports = router;
