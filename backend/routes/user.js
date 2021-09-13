1bc9e51e6da96288bd862f3faff3473483fb3799
Chris VanGroll <chrisvangroll@gmail.com>
2021-09-13 08:04:05 -0500

    password validation and bouncer

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const bouncer = require('express-bouncer')(500, 900000)
 //const user = require('../models/user');
 //const auth = require('../middleware/auth');
 
 const userCtrl = require('../controllers/user');
 
-const verifyPassword = require('../middleware/verifyPassword')
-
-router.post('/signup', verifyPassword, userCtrl.signup);
-router.post('/login', bouncer.block, userCtrl.login);
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
